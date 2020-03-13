662e7b0c6a178263f64c8ca0589c5a7b12103ce8
候胜斌 <shengbin1@staff.weibo.com>
2020-03-13 22:40:22 +0800

    update

--- web/src/main/java/com/xiangcunlu/web/confs/ErrorConfig.java
+++ web/src/main/java/com/xiangcunlu/web/confs/ErrorConfig.java
@@ -1,7 +1,6 @@
 package com.xiangcunlu.web.confs;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.ErrorPageRegistrar;
 import org.springframework.boot.web.server.ErrorPageRegistry;
