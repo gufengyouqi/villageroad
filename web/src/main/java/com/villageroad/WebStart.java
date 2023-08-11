edaba8494635a5ce13424de14c1b90b0449707bc
候胜斌 <houshengbin@houshengbindeMacBook-Pro.local>
2023-08-11 12:19:08 +0800

    添加线程池配置

--- web/src/main/java/com/villageroad/WebStart.java
+++ web/src/main/java/com/villageroad/WebStart.java
@@ -6,16 +6,21 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * @author houshengbin
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableAsync
 @EnableWebMvc
 //@EnableTransactionManagement
 //@EnableFeignClients(basePackages = {"com.villageroad.storage.feign"}, defaultConfiguration = {FeignClientConfig.class})
 public class WebStart {
     public static void main(String[] args) {
         SpringApplication.run(WebStart.class, args);
+        int coreSize = Runtime.getRuntime().availableProcessors();
+        // 启用并行GC
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", String.valueOf(coreSize));
     }
 }
